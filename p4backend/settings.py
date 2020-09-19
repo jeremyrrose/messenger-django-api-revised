88f2293a60a6e6bf14e6acdb2d32429379b6bee6
STUDIO SEMANTICA <m90studio@tutanota.com>
2020-09-18 21:10:53 -0400

    Added logion functionality

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
     'cloud_msg.apps.CloudMsgConfig',
     'rest_framework',
+    'corsheaders',
 
 
 ]
@@ -53,7 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'p4backend.urls'
 
